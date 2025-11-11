e7525a25563b3191cb4fce48117757064c5c2549
lxiscxstillo <luisestebancastillopedroza90@gmail.com>
2025-11-11 16:42:42 -0500

    cambio logo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'http://localhost:8081/api',
   whatsappNumber: '573001112233',
   showFloatingWhatsapp: false,
-  logoUrl: '/WhatsApp_Image_2025-08-20_at_6.58.17_AM-removebg-preview.png'
+  logoUrl: '/logo.png'
 };
