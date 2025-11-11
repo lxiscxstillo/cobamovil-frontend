804124830a4c6d21d9db8987da21cbcf94fd4a45
lxiscxstillo <luisestebancastillopedroza90@gmail.com>
2025-11-11 16:28:56 -0500

    actua

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://cobamovil-backend.onrender.com/api',
   showFloatingWhatsapp: false,
-  logoUrl: '/logo.png'
+  logoUrl: '/WhatsApp_Image_2025-08-20_at_6.58.17_AM-removebg-preview.png'
 };
