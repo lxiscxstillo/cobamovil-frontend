3fdb4a0b01d34acd7da6f15c16ae5be972503894
lxiscxstillo <luisestebancastillopedroza90@gmail.com>
2025-11-11 22:23:37 -0500

    arreglo

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Title, Meta } from '@angular/platform-browser';
